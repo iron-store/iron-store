e55a8dfb7ca3bf7782f4e92f4661218301f7bfa3
Daniel Lozano <lozanodaniel02@gmail.com>
2018-05-27 19:36:10 -0400

    add creation of categories and products in admin-area

--- angular/src/app/components/admin-area/admin-area.component.ts
+++ angular/src/app/components/admin-area/admin-area.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-admin-area',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminAreaComponent implements OnInit {
 
-  constructor() { }
+  productSeccion: boolean = false;
+  categorySeccion: boolean = false;
+  newCategory: Object = { parent: null };
+  newProduct: Object = {};
+  subCategoriesArray: any = [];
+  mainCategoriesArray: any = [];
+
+  constructor(private myCategories: CategoryService, private myProducts: ProductService) { }
 
   ngOnInit() {
+    this.subCategories();
+    this.mainCategories();
+  }
+
+  showCategory(): void {
+    this.productSeccion = false;
+    this.categorySeccion = !this.categorySeccion;
+  }
+
+  showProduct(): void {
+    this.categorySeccion = false;
+    this.productSeccion = !this.productSeccion;
+  }
+
+  subCategories() {
+    this.myCategories.getAllCategory()
+      .subscribe(
+        categories =>
+          categories.forEach(category => {
+            if (!category.parent)
+              this.subCategoriesArray.push(category)
+          }),
+        err => console.log(err)
+      )
+  }
+
+  addCategory() {
+    this.myCategories.createCategory(this.newCategory)
+      .subscribe(
+        newCat => console.log(newCat),
+        err => console.log(err)
+      )
+  }
+
+  mainCategories() {
+    this.myCategories.getAllCategory()
+      .subscribe(
+        categories =>
+          categories.forEach(category => {
+            if (category.parent)
+              this.mainCategoriesArray.push(category)
+          }),
+        err => console.log(err)
+      )
+  }
+
+  addProduct(){
+    this.myProducts.createProduct(this.newProduct)
+    .subscribe(
+      product => console.log(product),
+      err => console.log(err)
+    )
   }
 
 }
