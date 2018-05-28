90d71a7a12c17a45982951bc3532c8048a8a4b0b
Daniel Lozano <lozanodaniel02@gmail.com>
2018-05-28 15:27:53 -0400

    add delete and update item in cart and 'no parent category' opcion in creation of category

--- angular/src/app/components/cart/cart.component.ts
+++ angular/src/app/components/cart/cart.component.ts
@@ -9,10 +9,12 @@ import { CookieService } from '../../services/cookie.service';
 export class CartComponent implements OnInit {
 
   cartProducts: any;
+  orderInfo: Object;
 
   constructor(private myCookie: CookieService) { }
 
   ngOnInit() {
+    this.createOrderInfo();
     this.showCart();
   }
 
@@ -20,6 +22,47 @@ export class CartComponent implements OnInit {
     if (this.myCookie.getCookie('browser__settings')) {
       this.cartProducts = this.myCookie.getCookie('browser__settings');
     }
+  }
+
+  deleteProduct(productName: string): void {
+    let cookie = this.myCookie.getCookie('browser__settings')
+    for (let i = 0; i < cookie.length; i++) {
+      if (cookie[i].name === productName) {
+        cookie.splice(i, 1);
+        this.myCookie.setCookie('browser__settings', cookie, 1);
+        break;
+      }
+    }
+    this.showCart();
+    this.createOrderInfo();
+  }
+
+  updateCount(info: any): void {
+    let cookie = this.myCookie.getCookie('browser__settings')
+    for (let i = 0; i < cookie.length; i++) {
+      if (cookie[i].name === info.name) {
+        cookie[i].repeat = info.count;
+        this.myCookie.setCookie('browser__settings', cookie, 1);
+        break;
+      }
+    }
+    this.showCart();
+    this.createOrderInfo();
+  }
+
+  createOrderInfo(){
+    let subtotal = 0;
+    let tax = 6;
+    let taxAmount = 0;
+    let total = 0;
+
+    this.myCookie.getCookie('browser__settings').forEach(product => {
+      subtotal += product.price * product.repeat;
+    })
+    taxAmount = subtotal * tax / 100;
+    total = subtotal + taxAmount;
+
+    this.orderInfo = {subtotal, tax, taxAmount, total}
   }
 
 }
